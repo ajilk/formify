e2fccd8caea08b5ad374e8f82a2b1adab20de72b
Eldor Bekpulatov <eb654@cornell.edu>
2018-09-03 21:12:02 -0400

    First sample form

--- myProject/views.py
+++ myProject/views.py
@@ -1,7 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from .forms import NameForm
 
 
 def index(request):
-	return render(request, 'index.html');
-	# return HttpResponse("Hello World")
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            return HttpResponseRedirect('')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'index.html', {'form': form})
